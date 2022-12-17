9a68403a5085f9be98e6fb61d59f7168362bccea
augustus_fx <agbolanaaugustine@gmail.com>
2022-12-17 10:41:17 -0800

    monty task

---
+++ rotr.c
@@ -0,0 +1,27 @@
+/**
+ *f_rotr- rotates the stack to the bottom
+ *@head: stack head
+ *f_rotr- rotates the stack to the bottom
+ *@head: stack head
+ *@counter: line_number
+ *Return: no return
+*/
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*heah)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
